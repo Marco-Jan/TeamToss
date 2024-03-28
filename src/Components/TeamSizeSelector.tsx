e602b77e9cbc44021216c44cd7ac943e5f05e979
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-03-28 13:19:01 +0100

    first db commit

---
+++ src/Components/TeamSizeSelector.tsx
@@ -0,0 +1,23 @@
+
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+
+function TeamSizeSelector({ teamSize, setTeamSize }: { teamSize: string, setTeamSize: (value: string) => void}) {
+    return (
+    <FormControl fullWidth sx={{ margin: '20px 0' }}>
+        <InputLabel id="team-size-label">Welche Team Größe soll erstellt werden?</InputLabel>
+        <Select
+            labelId="team-size-label"
+            id="TeamChoiceID"
+            value={teamSize}
+            label="Welche Team Größe soll erstellt werden?"
+            onChange={(e) => setTeamSize(e.target.value)}
+        >
+            <MenuItem value="Team2">2 Teams</MenuItem>
+            <MenuItem value="Team3">3 Teams</MenuItem>
+            <MenuItem value="Team4">4 Teams</MenuItem>
+        </Select>
+    </FormControl>
+    );
+  }
+  
+    export default TeamSizeSelector;
