dbc8798c7945af47a1f1ca58fe3e07933acf245d
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-03-30 12:40:44 +0100

    DesignCommit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './Components/Thema/theme'; // Importiere das Theme, das du erstellt hast
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
