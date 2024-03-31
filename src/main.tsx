7baf2e1069478143f9391604e46902a80738f535
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-03-31 16:04:08 +0200

    merge and repair

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './Components/Thema/theme'; // Importiere das Theme, das du erstellt hast
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -11,11 +9,7 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+)
